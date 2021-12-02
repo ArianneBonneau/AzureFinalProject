4fb02b537446a961417387857fc733eb3ccec933
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-02 13:15:26 -0500

    implemented roles

---
+++ CollegeManagementServices/CollegeManagementServices/Data/Migrations/20211202180648_updatingDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CollegeManagementServices.Data.Migrations
+{
+    public partial class updatingDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
