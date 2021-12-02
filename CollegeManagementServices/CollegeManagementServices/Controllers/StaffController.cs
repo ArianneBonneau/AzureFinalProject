ea69f547e220a1e9aa6f4e756e2163f755e723b0
Adam Mihajlovic <mihajlovicadam@gmail.com>
2021-12-01 22:00:27 -0500

    Added Staff and Student Page

---
+++ CollegeManagementServices/CollegeManagementServices/Controllers/StaffController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeManagementServices.Controllers
+{
+    public class StaffController : Controller
+    {
+        [Authorize(Roles="Staff")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CollegeManagementServices/CollegeManagementServices/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeManagementServices.Controllers
+{
+    public class StudentController : Controller
+    {
+        [Authorize(Roles="Student")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
